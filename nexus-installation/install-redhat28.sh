b5e81a620ecf9d44dc425bf141f9e5a1b7b77b02
Wilfred Okoro <101127016+wilfredokoro@users.noreply.github.com>
2022-09-23 08:33:10 -0500

    Update install-redhat28.sh

--- nexus-installation/install-redhat28.sh
+++ nexus-installation/install-redhat28.sh
@@ -6,7 +6,7 @@
 #1. Create nexus user to manage the nexus
 # As a good security practice, Nexus is not advised to run nexus service as a root user, so create a new user called nexus and grant sudo access to manage nexus services as follows.
 sudo hostnamectl hostname nexus
-useradd nexus
+sudo useradd nexus
 #4 Give sudo access to nexus user
 sudo echo "nexus ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/nexus
 sudo su - nexus
